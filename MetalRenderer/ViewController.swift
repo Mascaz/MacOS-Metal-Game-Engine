a91086f280a9ffd16d4733bb17e0c45698170e94
Maxwell Montes Diaz <maxwell_montes@yahoo.com>
2021-02-25 00:47:13 -0800

    Setting up metal project files, pipeline and renderer

--- MetalRenderer/ViewController.swift
+++ MetalRenderer/ViewController.swift
@@ -6,19 +6,24 @@
 //
 
 import Cocoa
+import MetalKit
 
 class ViewController: NSViewController {
-
+    @IBOutlet var metalView: MTKView!
+    var renderer: Renderer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        renderer = Renderer(view: metalView)
+        metalView.device = Renderer.device
+        metalView.delegate = renderer
+        
+        metalView.clearColor = MTLClearColor(red: 1.0, green: 1.0, blue: 0.8, alpha: 1.0)
+        
+        
 
         // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
 
 
