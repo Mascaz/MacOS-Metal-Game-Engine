52c413ab9337c3b1060f6965bc9d91013c32508d
Maxwell Montes Diaz <maxwell_montes@yahoo.com>
2021-02-25 00:25:15 -0800

    Initial Commit

---
+++ MetalRenderer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MetalRenderer
+//
+//  Created by Maxwell Montes Diaz on 2/25/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
